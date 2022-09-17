17464b07b902aa14f540aa491f0334b0d2ff91a8
高 <75872353+15332606214@users.noreply.github.com>
2022-09-17 17:37:44 +0800

    Add files via upload

---
+++ vue/vue-basic/3-Vue脚手架/03_src_props属性/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <StudentCom name="李四" sex="女" :age="18"/>
+    </div>
+</template>
+
+<script>
+    // 引入School组件
+    import StudentCom from "./components/StudentCom.vue";
+    export default {
+        name:'App',
+        components:{StudentCom},
+    }
+</script>
