72ec7de66051d917ccf465683a4246ed435e9a54
高 <75872353+15332606214@users.noreply.github.com>
2022-09-19 07:48:57 +0800

    Add files via upload

---
+++ vue/vue-basic/3-Vue脚手架/07_src_TodoList案例(第一版)/components/MyList.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul class="todo-main">
+    <myItem 
+    v-for="todoObj in todos" 
+    :key="todoObj.id" 
+    :todo="todoObj" 
+    :checkTodo="checkTodo"
+    :deleteTodo="deleteTodo"
+    />
+    <!-- v-for="todoObj in todos" :key="todoObj.id"根据数据判断出现多少个myItem -->
+  </ul>
+</template>
+
+<script>
+import MyItem from './MyItem.vue'
+
+export default {
+    name:'MyList',
+    components: { MyItem },
+    props:['todos','checkTodo','deleteTodo']
+}
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
