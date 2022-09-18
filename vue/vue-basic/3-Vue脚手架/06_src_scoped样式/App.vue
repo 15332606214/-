ee11b5303f89c9b1ee0431f369075a24e075628c
高 <75872353+15332606214@users.noreply.github.com>
2022-09-19 07:48:30 +0800

    Add files via upload

---
+++ vue/vue-basic/3-Vue脚手架/06_src_scoped样式/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1 class="title">你好啊！</h1>
+        <SchoolCom/>
+        <StudentCom/>
+    </div>
+</template>
+
+<script>
+import SchoolCom from './components/SchoolCom.vue';
+import StudentCom from "./components/StudentCom.vue";
+    export default {
+        name:'App',
+        components:{
+            StudentCom, 
+            SchoolCom
+        },
+    }
+</script>
+// 加scoped属性后只有本页内容可以使用属性，引入的内容无法使用此样式
+<style scoped>
+    .title{
+        color:red;
+    }
+</style>
---
+++ vue/vue-basic/3-Vue脚手架/06_src_scoped样式/components/SchoolCom.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="demo">
+        <h2 class="title">学校名称：{{name}}</h2>
+        <h2 class="qwe">学校地址：{{address}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'SchoolCom',
+    data(){
+        return{
+            name:'小尚',
+            address:'北京'
+        }
+    }
+}
+</script>
+<style lang='less'>
+    .demo{
+        background-color: skyblue;
+        .qwe{
+            font-size: 40px;
+        }
+    }
+</style>
